8ba1ac95acc80af6593c458fddbd4f89eef0e896
Maryam Anileleye <maryamanileleye@gmail.com>
2026-05-18 16:48:55 +0100

    Did exercise numberChecker
    All test passed

--- foundations/03_numberChecker/numberChecker.js
+++ foundations/03_numberChecker/numberChecker.js
@@ -1,10 +1,12 @@
 function numberChecker(number) {
-  if (number === 6) {
+  if (number >= 10) {
+    console.log(true);
     return true;
   } else {
+    console.log(false);
     return false;
   }
 }
-
+numberChecker(6)
 // Do not edit below this line
 module.exports = numberChecker;
