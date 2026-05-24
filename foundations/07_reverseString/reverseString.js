afb6e120128ec94ca9bedbab645b913b58cddb92
Maryam Anileleye <maryamanileleye@gmail.com>
2026-05-24 13:13:56 +0100

    Did exercise reverseString

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,6 +1,13 @@
-const reverseString = function() {
+const reverseString = function(word) {
+    word_reverse = " "
+    for (num = 1; num <= word.length; num++) {
+        neg_index = -num;
+        word_index = word.at(neg_index)
+        word_reverse += word_index
 
+    }
+    console.log(word_reverse)
 };
-
+reverseString("malik")
 // Do not edit below this line
 module.exports = reverseString;
