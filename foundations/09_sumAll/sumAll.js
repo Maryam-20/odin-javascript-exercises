5188fff129aaf78b66709b777cfef92ae6b953e1
Maryam Anileleye <maryamanileleye@gmail.com>
2026-05-24 16:06:45 +0100

    Did exercise sumAll

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,6 +1,10 @@
-const sumAll = function() {
-
+const sumAll = function(a, b) {
+    number = 0;
+    for (let num = 1; num <= b; num++ ) {
+        number += num 
+    };
+    return number;
 };
-
+console.log(sumAll(1, 4))
 // Do not edit below this line
 module.exports = sumAll;
