0e9438f069f49eac543962b4dc5827a5cf76100e
Maryam Anileleye <maryamanileleye@gmail.com>
2026-05-24 16:31:53 +0100

    Did exercise leapYears

--- foundations/10_leapYears/leapYears.js
+++ foundations/10_leapYears/leapYears.js
@@ -1,6 +1,10 @@
-const leapYears = function() {
-
-};
-
+const leapYears = function(year) {
+    if (year % 4 === 0 || year % 400 === 0 ) {
+        return "Leap Year " + true
+    }else {
+        return "Is not a Leap Year " + false
+    }
+}
+console.log(leapYears(1985))
 // Do not edit below this line
 module.exports = leapYears;
