73fcc8c2d5a34fe1ef8c1244c8c48cddf974f7e3
Maryam Anileleye <maryamanileleye@gmail.com>
2026-05-24 18:48:09 +0100

    Did exercise tempConversion

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,9 +1,15 @@
-const convertToCelsius = function() {
-};
+const convertToCelsius = function(farenheit) {
+  const farenheit_2_celcius = Math.round((farenheit -32) * 5/9)
+  console.log(farenheit_2_celcius)
+}
+convertToCelsius(92)
 
-const convertToFahrenheit = function() {
-};
 
+const convertToFahrenheit = function(celcius) {
+  const celcius_2_farenheit = Math.round((celcius * 9/5) + 32)
+  console.log(celcius_2_farenheit)
+};
+convertToFahrenheit(33.333)
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
