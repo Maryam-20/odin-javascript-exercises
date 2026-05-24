61b7804f0436b0284e93bce261fd8ea52b6f1fc7
Maryam Anileleye <maryamanileleye@gmail.com>
2026-05-24 14:49:03 +0100

    Did exercise removeFromArray

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,5 +1,22 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...args) {
+    const removed_array = array.filter((item) =>!args.includes(item))
+    console.log(removed_array)
+    
+    // console.log(removed_array)
+    // console.log(newArray)
 };
-
-// Do not edit below this line
+removeFromArray([1, 6, 2, 2, 6, 7, 4, 2, 2, 6, 9], 6)
+// // Do not edit below this line
 module.exports = removeFromArray;
+// let newArray = []
+// const removeFromArray = function(array, ...args) {
+//     array.forEach((item)=> {
+//         // console.log(item)
+//         // console.log(args)
+//         if(!args.includes(item)) {
+//             newArray.push(item)
+//         }
+//     });
+//     console.log(newArray)
+// };
+// removeFromArray([2, 3, 4, 3, 4, 5, 6, 3, 6], 3)
