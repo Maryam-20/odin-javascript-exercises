5e42da7ca530c5f1b7bee282f71192d935567c7f
Maryam Anileleye <maryamanileleye@gmail.com>
2026-05-24 12:07:18 +0100

    Did exercise repeatString

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,6 +1,20 @@
-const repeatString = function() {
 
+const repeatString = function(str, val) {
+    num = 0
+    let repeat_str = " "
+    if (val < 0){
+        console.log("ERRor")
+    }else{
+        while (num < val) {
+            repeat_str += str
+            console.log("vxdfhsss")
+
+            num++;
+        }
+    };
+    console.log(repeat_str)
 };
 
+repeatString("hey", 4)
 // Do not edit below this line
 module.exports = repeatString;
